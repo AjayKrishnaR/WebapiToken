5511a0102541828ce4b17bbe7cd88e53ed1d24b8
SUYATITECH\akramachandran <ajay.4686.sskzm@gmail.com>
2017-09-06 17:18:11 +0530

    Add project files.

---
+++ WebApiToken/Controllers/Api/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiToken.Filters;
+using WebApiToken.Models;
+
+namespace WebApiToken.Controllers.Api
+{
+    public class AuthController : ApiController
+    {
+
+
+
+
+        BusinessService _BusinessService = new BusinessService();
+
+        private HttpResponseMessage GetAuthToken(string username, string password)
+        {
+            var token = _BusinessService.GenerateToken(username, password);
+            var response = Request.CreateResponse(HttpStatusCode.OK, "Authorized");
+            response.Headers.Add("Token", token.Authtoken);
+            response.Headers.Add("TokenExpiry", DateTime.Now.AddHours(2).ToString());
+            response.Headers.Add("Access-Control-Expose-Headers", "Token,TokenExpiry");
+            return response;
+        }
+
+        [Route("Authenticating")]
+        [HttpGet]
+        public HttpResponseMessage Authenticate(string username, string password)
+        {
+            return GetAuthToken(username, password);
+        }
+
+        [AuthorizationRequired]
+        [Route("GetUserAgain")]
+        public  string GetUserDetails(string username)
+        {
+            return "Invalid";
+        }
+    }
+}
---
+++ WebApiToken/Models/BusinessService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiToken.Models
+{
+    public class BusinessService : Iservice
+    {
+     
+
+        public WebApiToken GenerateToken(String username, string password)
+        {
+            string token = Guid.NewGuid().ToString();
+            DateTime issuedOn = DateTime.Now;
+            DateTime expiredOn = DateTime.Now.AddMinutes(5);
+            var tokendomain = new WebApiToken
+            {
+                Username = username,
+                Authtoken = token,
+                IssuedOn = issuedOn,
+                ExpiryOn = expiredOn
+            };
+            WebApiToken tk = new WebApiToken();
+            StoreDbEntities sd = new StoreDbEntities();
+            tk = sd.WebApiTokens.Where(x => x.Username == username & x.Password == password).FirstOrDefault();
+            if (tk.Username != null)
+            {
+                tk.Authtoken = tokendomain.Authtoken;
+                tk.IssuedOn = tokendomain.IssuedOn;
+                tk.ExpiryOn = tokendomain.ExpiryOn;
+                sd.SaveChanges();
+            }
+            return tokendomain;
+        }
+
+
+
+        internal bool ValidateToken(string tokenValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        WebApiToken Iservice.ValidateToken(string tokenValue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
